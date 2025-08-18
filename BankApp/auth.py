7c4073395801e68e35c042672a7646ec50ff6f97
[Mark Laurence Batiola] <marklaurence355@gmail.com>
2025-08-18 16:09:22 +0800

    created a folder for auth and transaction

---
+++ BankApp/auth.py
@@ -0,0 +1,60 @@
+import getpass as gp
+
+isLoggedIn = False
+def register():
+    print("===Register===")
+    username = input("Enter your username:")
+    pin = gp.getpass("Enter your PIN:")
+    
+    if pin.isdigit() and len(pin) == 4:
+        print("PIN Accepted. Ready to log-in!")
+        print(username, "\n", pin)
+        return username, pin
+
+    else:
+        print("Invalid input. please exactly put 4 digits")
+        return None, None
+   
+        
+def login(username, pin):
+    while True:
+        print("===LOGIN===")
+        userUsernameInput = input("Enter your username:")
+        userPinInput = gp.getpass("Enter your PIN:")
+    
+        if userUsernameInput != username:
+            print("Incorrect username, please be sure you registered your account.")
+            break
+        
+        if userPinInput != pin:
+            print("Incorrect PIN input!")
+            break
+        
+        print("Login Successful!")
+        isLoggedIn == True
+        break
+        
+        
+def landingPage():
+    while True:
+        print("1.Register \n2.Login \n3.Exit")
+        choice = input("Enter your choice:")
+        if choice == '1':
+            username, pin = register()
+        elif choice == '2':
+            if username and pin:
+                login(username, pin)
+                if(isLoggedIn):
+                    print("Logged In")
+            else:
+             print("No users registered yet.")
+        elif choice == '3':
+            print("Exiting...")
+            exit()
+        else:
+            print("Please input 1-3 only.")
+            break
+        
+
+
+landingPage()
